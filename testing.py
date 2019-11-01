4b94fd0d44fa4af5d00957c7bc9a48d30f08edda
Ryan Hosford <hosfordryan@gmail.com>
2019-11-01 15:43:44 -0500

    Added facial landmark detection to testing.py

--- testing.py
+++ testing.py
@@ -28,19 +28,52 @@ def main():
     #     face.show()
 
     #TODO: Download predictor and set path (https://www.pyimagesearch.com/2017/04/10/detect-eyes-nose-lips-jaw-dlib-opencv-python/)
-    path_to_shape_predictor = ""
+    path_to_shape_predictor = "shape_predictor_68_face_landmarks.dat"
 
     # init dlib's face detector (HOG-based) and then create the facial landmark predictor
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(path_to_shape_predictor)
 
     # load input image
-    img = Image("~/Pictures/PicOfTheDay-Testing/IMG_20190911_210847.jpg")
+    #img = Image("~/Pictures/PicOfTheDay-Testing/IMG_20190911_210847.jpg")
+    img = Image("testImage.jpg")
     img = imutils.resize(img.get(), width=500)
-    gray = cv2.cvtColor(img.get(), cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     #detect faces in grayscale image
     rects = detector(gray, 1)
+
+    #loop over the face detections
+    for (i, rect) in enumerate(rects):
+        #determine the facial landmarks for the face region, then convert coords to NumPy array
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        #loop over the face parts individually
+        for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
+            #clone original image so we can draw on it then display name of face part
+            clone = img.copy()
+            cv2.putText(clone, name, (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+            #loop over subset of facial landmarks, drawing the specific face part
+            for (x, y) in shape[i:j]:
+                cv2.circle(clone, (x, y), 1, (0, 0, 255), -1)
+            
+            #extract the ROI of the face region as a separate image
+            (x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
+            roi = img[y:y + h, x:x + w]
+            roi = imutils.resize(roi, width=250, inter=cv2.INTER_CUBIC)
+
+
+            #show the particular face part
+            cv2.imshow("ROI", roi)
+            cv2.imshow("IMAGE", clone)
+            cv2.waitKey(0)
+
+        #visualize all facial landmarks with a transparent overlay
+        output = face_utils.visualize_facial_landmarks(img, shape)
+        cv2.imshow("Image", clone)
+        cv2.waitKey(0)
 
 if __name__ == "__main__":
     main()
